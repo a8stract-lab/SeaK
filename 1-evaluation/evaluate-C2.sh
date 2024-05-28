61a9ba124036264e03446dff3f19496d21108206
201220007 <85940970+201220007@users.noreply.github.com>
2024-05-28 10:47:15 +0800

    modify scripts and ReadMe

--- 1-evaluation/evaluate-C2.sh
+++ 1-evaluation/evaluate-C2.sh
@@ -9,7 +9,7 @@ unzip lmbench.zip
 gnome-terminal --title="terminal1: RUN VM & POC here"  -- ./run-C2.sh 
 sleep 20
 #./copy2vm.sh ../2-source-code/linux-5.15.106/sample/bpf
-./copy2vm.sh ./2-source-code/POCs
+./copy2vm.sh ../2-source-code/POCs
 ./copy2vm.sh ./scripts
 gnome-terminal --title="terminal2: RUN BPF & MONITOR OUTPUT here" -- ./connect2vm.sh
 
