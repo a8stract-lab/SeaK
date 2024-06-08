f5a954394e9ef9e8af1cef5ceb415fd54fd6fba7
201220007 <85940970+201220007@users.noreply.github.com>
2024-06-08 20:54:46 +0800

    gyc

--- 1-evaluation/evaluate-C3.sh
+++ 1-evaluation/evaluate-C3.sh
@@ -8,7 +8,7 @@ unzip lmbench.zip
 
 gnome-terminal --title="terminal1: RUN VM & POC here"  -- ./run-C3.sh 
 sleep 20
-./copy2vm.sh ../2-source-code/linux-5.15.106/samples/bpf
+./copy2vm.sh ./bpf-evaluation
 ./copy2vm.sh ../2-source-code/POCs
 ./copy2vm.sh ./scripts
 gnome-terminal --title="terminal2: RUN BPF & MONITOR OUTPUT here" -- ./connect2vm.sh
